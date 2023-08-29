2d38eb50620abafa7fe8e3212a96796548d21ab7
Jannawant <tsaruevfelix@gmail.com>
2023-08-29 21:02:21 +0300

    chore: domain-service take 2

--- cmd/port-service/main.go
+++ cmd/port-service/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"portsApi/internal/config"
+	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"portsApi/internal/config"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -14,6 +22,42 @@ func main() {
 }
 
 func run() error {
-	_ = config.Read()
+	cfg := config.Read()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(map[string]bool{
+			"pong": true,
+		})
+	}).Methods("GET")
+
+	srv := &http.Server{
+		Addr:    cfg.HTTPAddr,
+		Handler: router,
+	}
+	// listen to OS signals and gracefully shutdown HTTP server
+	stopped := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(stopped)
+	}()
+
+	log.Printf("Starting HTTP server on %s", cfg.HTTPAddr)
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe Error: %v", err)
+	}
+
+	<-stopped
+	log.Printf("Goodbye")
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module portsApi
 
 go 1.21.0
+
+require github.com/gorilla/mux v1.8.0 // indirect
